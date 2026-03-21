fa7711156af4696256e3a65c3bf81dde7c50b630
Pelemenguin <elepentuin@outlook.com>
2026-03-21 22:02:44 +0800

    Add editor for `HSVPointFilter`s

--- app/src/main/java/pelemenguin/texturegen/api/builtin/HSVPointFilter.java
+++ app/src/main/java/pelemenguin/texturegen/api/builtin/HSVPointFilter.java
@@ -1,10 +1,17 @@
 package pelemenguin.texturegen.api.builtin;
 
 import java.awt.image.BufferedImage;
+import java.util.function.Consumer;
 
 import com.google.gson.annotations.SerializedName;
 
+import pelemenguin.texturegen.api.client.terminal.ANSIHelper;
+import pelemenguin.texturegen.api.client.terminal.StringInput;
+import pelemenguin.texturegen.api.client.terminal.TerminalMenu;
+import pelemenguin.texturegen.api.client.terminal.TerminalMenuContext;
+import pelemenguin.texturegen.api.client.terminal.TerminalPointFilterEditorProvider;
 import pelemenguin.texturegen.api.util.ColorHelper;
+import pelemenguin.texturegen.api.util.CommonRegistry;
 import pelemenguin.texturegen.api.util.JsonRegistry;
 import pelemenguin.texturegen.api.util.PointFilter;
 
@@ -25,12 +32,101 @@ public sealed abstract class HSVPointFilter implements PredicatePointFilter
     protected transient int hsvBand;
     protected transient String hsvName;
 
+    protected transient int MAX;
+    protected transient int MIN;
+
     protected boolean testValue(int value) {
-        boolean lowerPass = lowerBoundInclusive ? value >= lowerBound : value > lowerBound;
-        boolean upperPass = upperBoundInclusive ? value <= upperBound : value < upperBound;
+        boolean lowerPass = (invert ^ lowerBoundInclusive) ? value >= lowerBound : value > lowerBound;
+        boolean upperPass = (invert ^ upperBoundInclusive) ? value <= upperBound : value < upperBound;
         return invert ^ (lowerPass && upperPass);
     }
-        
+
+    public static sealed abstract class TerminalEditor<F extends HSVPointFilter> implements TerminalPointFilterEditorProvider, TerminalPointFilterEditorProvider.Editor<F>
+        permits Hue.TerminalEditor, Saturation.TerminalEditor, Value.TerminalEditor {
+
+        protected String hsvName;
+
+        @Override
+        public void editorLoop(F pointFilter, Consumer<F> setter, TerminalMenuContext context) {
+            TerminalMenu menu = new TerminalMenu("")
+                .autoUppercase();
+            menu.addKey('-', "Back")
+                .addKey('L', "Lower Bound: " + ANSIHelper.blue(String.valueOf(pointFilter.lowerBound)), () -> {
+                    menu.updateKeyDescription('L', "Lower Bound: " + ANSIHelper.bold(ANSIHelper.blue("> " + pointFilter.lowerBound + " <")));
+                    String result = new StringInput(menu.getDisplayContent()).scan(context);
+                    try {
+                        int newLowerBound = Integer.parseInt(result);
+                        if (newLowerBound < pointFilter.MIN || newLowerBound > pointFilter.upperBound) {
+                            throw new NumberFormatException();
+                        }
+                        pointFilter.lowerBound = newLowerBound;
+                        setter.accept(pointFilter);
+                    } catch (NumberFormatException e) {
+                        context.outStream().println(ANSIHelper.red("Invalid input. Please enter an integer within %d ~ %d".formatted(pointFilter.MIN, pointFilter.upperBound)));
+                    }
+                    menu.updateKeyDescription('L', "Lower Bound: " + ANSIHelper.blue(String.valueOf(pointFilter.lowerBound)));
+                })
+                .addKey('U', "Upper Bound: " + ANSIHelper.blue(String.valueOf(pointFilter.upperBound)), () -> {
+                    menu.updateKeyDescription('U', "Upper Bound: " + ANSIHelper.bold(ANSIHelper.blue("> " + pointFilter.upperBound + " <")));
+                    String result = new StringInput(menu.getDisplayContent()).scan(context);
+                    try {
+                        int newUpperBound = Integer.parseInt(result);
+                        if (newUpperBound > pointFilter.MAX || newUpperBound < pointFilter.lowerBound) {
+                            throw new NumberFormatException();
+                        }
+                        pointFilter.upperBound = newUpperBound;
+                        setter.accept(pointFilter);
+                    } catch (NumberFormatException e) {
+                        context.outStream().println(ANSIHelper.red("Invalid input. Please enter an integer within %d ~ %d".formatted(pointFilter.lowerBound, pointFilter.MAX)));
+                    }
+                    menu.updateKeyDescription('U', "Upper Bound: " + ANSIHelper.blue(String.valueOf(pointFilter.upperBound)));
+                })
+                .addKey('W', "Lower Bound Inclusive: " + ANSIHelper.blue(String.valueOf(pointFilter.lowerBoundInclusive)), () -> {
+                    pointFilter.lowerBoundInclusive = !pointFilter.lowerBoundInclusive;
+                    menu.updateKeyDescription('W', "Lower Bound Inclusive: " + ANSIHelper.blue(String.valueOf(pointFilter.lowerBoundInclusive)));
+                    setter.accept(pointFilter);
+                })
+                .addKey('P', "Upper Bound Inclusive: " + ANSIHelper.blue(String.valueOf(pointFilter.upperBoundInclusive)), () -> {
+                    pointFilter.upperBoundInclusive = !pointFilter.upperBoundInclusive;
+                    menu.updateKeyDescription('P', "Upper Bound Inclusive: " + ANSIHelper.blue(String.valueOf(pointFilter.upperBoundInclusive)));
+                    setter.accept(pointFilter);
+                })
+                .addKey('I', "Invert: " + ANSIHelper.blue(String.valueOf(pointFilter.invert)), () -> {
+                    pointFilter.invert = !pointFilter.invert;
+                    menu.updateKeyDescription('I', "Invert: " + ANSIHelper.blue(String.valueOf(pointFilter.invert)));
+                    setter.accept(pointFilter);
+                });
+            while (true) {
+                int barLength = 50;
+                int charIndexOfLowerBound = (int) ((pointFilter.lowerBound - pointFilter.MIN) / (double) (pointFilter.MAX - pointFilter.MIN) * barLength);
+                int charIndexOfUpperBound = (int) ((pointFilter.upperBound - pointFilter.MIN) / (double) (pointFilter.MAX - pointFilter.MIN) * barLength);
+                String partBar1 = (pointFilter.invert ? "=" : "-").repeat(Math.max(0, charIndexOfLowerBound));
+                String partBar2 = (pointFilter.invert ? "-" : "=").repeat(Math.max(0, charIndexOfUpperBound - charIndexOfLowerBound - 1));
+                String partBar3 = (pointFilter.invert ? "=" : "-").repeat(Math.max(0, barLength - charIndexOfUpperBound - 1));
+                String split1 = (pointFilter.lowerBoundInclusive ? "|" : (pointFilter.invert ? ")" : "("));
+                String split2 = (pointFilter.upperBoundInclusive ? "|" : (pointFilter.invert ? "(" : ")"));
+                String bar;
+                if (pointFilter.invert) {
+                    bar = ANSIHelper.green(partBar1 + split1) + ANSIHelper.red(partBar2) + ANSIHelper.green(split2 + partBar3);
+                } else {
+                    bar = ANSIHelper.red(partBar1) + ANSIHelper.green(split1 + partBar2 + split2) + ANSIHelper.red(partBar3);
+                }
+                menu.updateDescription(pointFilter.getPointFilterTitle() + "\n\n" + bar + "\n");
+
+                char choice = menu.scan(context);
+                if (choice == '-') {
+                    break;
+                }
+            }
+        }
+
+        @Override
+        public Editor<? extends PointFilter> getEditor() {
+            return this;
+        }
+
+    }
+
     @Override
     public boolean testPoint(BufferedImage image, int x, int y) {
         int[] temp = new int[3];
@@ -63,8 +159,10 @@ public sealed abstract class HSVPointFilter implements PredicatePointFilter
     public static final class Hue extends HSVPointFilter {
 
         public Hue() {
-            this.lowerBound = 0;
-            this.upperBound = 360;
+            this.MIN = 0;
+            this.MAX = 360;
+            this.lowerBound = this.MIN;
+            this.upperBound = this.MAX;
             this.hsvBand = 0;
             this.hsvName = "Hue";
         }
@@ -74,13 +172,28 @@ public sealed abstract class HSVPointFilter implements PredicatePointFilter
             registry.register("texturegen.hsv.hue", Hue.class);
         }
 
+        public static final class TerminalEditor extends HSVPointFilter.TerminalEditor<Hue> {
+
+            public TerminalEditor() {
+                this.hsvName = "Hue";
+            }
+
+            @Override
+            public void register(CommonRegistry<TerminalPointFilterEditorProvider> registry) {
+                registry.register("texturegen.hsv.hue", this);
+            }
+
+        }
+
     }
 
     public static final class Saturation extends HSVPointFilter {
 
         public Saturation() {
-            this.lowerBound = 0;
-            this.upperBound = 100;
+            this.MIN = 0;
+            this.MAX = 100;
+            this.lowerBound = this.MIN;
+            this.upperBound = this.MAX;
             this.hsvBand = 1;
             this.hsvName = "Saturation";
         }
@@ -90,13 +203,28 @@ public sealed abstract class HSVPointFilter implements PredicatePointFilter
             registry.register("texturegen.hsv.saturation", Saturation.class);
         }
 
+        public static final class TerminalEditor extends HSVPointFilter.TerminalEditor<Saturation> {
+
+            public TerminalEditor() {
+                this.hsvName = "Saturation";
+            }
+
+            @Override
+            public void register(CommonRegistry<TerminalPointFilterEditorProvider> registry) {
+                registry.register("texturegen.hsv.saturation", this);
+            }
+
+        }
+
     }
 
     public static final class Value extends HSVPointFilter {
 
         public Value() {
-            this.lowerBound = 0;
-            this.upperBound = 100;
+            this.MIN = 0;
+            this.MAX = 100;
+            this.lowerBound = this.MIN;
+            this.upperBound = this.MAX;
             this.hsvBand = 2;
             this.hsvName = "Value";
         }
@@ -104,6 +232,19 @@ public sealed abstract class HSVPointFilter implements PredicatePointFilter
         @Override
         public void register(JsonRegistry<PointFilter> registry) {
             registry.register("texturegen.hsv.value", Value.class);
+        }
+
+        public static final class TerminalEditor extends HSVPointFilter.TerminalEditor<Value> {
+
+            public TerminalEditor() {
+                this.hsvName = "Value";
+            }
+
+            @Override
+            public void register(CommonRegistry<TerminalPointFilterEditorProvider> registry) {
+                registry.register("texturegen.hsv.value", this);
+            }
+
         }
 
     }
